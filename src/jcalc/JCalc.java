050614934d99548cbf1544e34d76ceee542f9253
Mathias Stemen <mstemen@gmail.com>
2015-02-27 13:19:12 -0600

    Added system icon

--- src/jcalc/JCalc.java
+++ src/jcalc/JCalc.java
@@ -6,14 +6,10 @@
 package jcalc;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -28,7 +24,8 @@ public class JCalc extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("JCalcFXML.fxml"));
         
         Scene scene = new Scene(root);
-        
+        Image systemImage = new Image(getClass().getResourceAsStream("/jcalc/images/calcfx_icon_from_vintage_casio_64.jpg"));
+        stage.getIcons().add(systemImage);
         stage.setScene(scene);
         stage.show();
     }
